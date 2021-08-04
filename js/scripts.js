1a0d22328e025f994cecd9f5af4d6724c943aea4
UkiePro <ukiepro@UkiePros-iMac.local>
2021-08-04 14:01:44 +0300

    f

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ $( document ).ready(function() {
         var posY = offset.top - $(window).scrollTop();
         console.log($(this).scrollTop(), posY)
         if ($(window).scrollTop() != 0) {
-            $('#bg_text_1').css('transform', 'translateX(-' + (2980 - $(window).scrollTop() * 0.5) +'px)');
+            $('#bg_text_1').css('transform', 'translateX(-' + (2980 - $(window).scrollTop() * 0.2) +'px)');
         } else {
             $('#bg_text_1').removeAttr("style");
         }
