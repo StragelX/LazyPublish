fb17376d89f82575d68a1a146b342c2960dae32a
UkiePro <ukiepro@UkiePros-iMac.local>
2021-08-04 18:55:19 +0300

    f

---
+++ .history/js/scripts_20210804185018.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+    $(window).scroll(function() {
+        if ($(window).scrollTop() != 0) {
+            $('#bg_text_1').css('transform', 'translateX(-' + (2980 - $(window).scrollTop() * 0.2) + 'px)');
+            $('#bg_text_2').css('transform', 'translateX(-' + (1000 - $(window).scrollTop() * 0.2) + 'px)');
+            $('.img_2').css('transform', 'translateY(' + (($(window).scrollTop() / 4.3) - 430) + 'px)');
+            $('.img_4').css('transform', 'translateY(' + (($(window).scrollTop() / 8) - 270) + 'px)');
+            $('.improve_bg').css('transform', 'translateY(' + ($(window).scrollTop() / 9.5) + 'px)');
+            // $('.img_0').css('transform', 'translateY(' + ($(window).scrollTop() / 8) + 'px)');
+        } else {
+            $('#bg_text_1, .improve_bg, .img_0').removeAttr("style");
+        }
+    })
+});
