4a3f0d9c2a7cdd65d8b4dd83da90c4bbf0c70830
UkiePro <ukiepro@UkiePros-iMac.local>
2021-08-04 18:49:52 +0300

    f

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,8 @@ $( document ).ready(function() {
         if ($(window).scrollTop() != 0) {
             $('#bg_text_1').css('transform', 'translateX(-' + (2980 - $(window).scrollTop() * 0.2) + 'px)');
             $('#bg_text_2').css('transform', 'translateX(-' + (1000 - $(window).scrollTop() * 0.2) + 'px)');
-            $('.img_2').css('transform', 'translateY(' + (($(window).scrollTop() / 4.3) - 500) + 'px)');
-            $('.img_4').css('transform', 'translateY(' + (($(window).scrollTop() / 8) - 500) + 'px)');
+            $('.img_2').css('transform', 'translateY(' + (($(window).scrollTop() / 4.3) - 430) + 'px)');
+            $('.img_4').css('transform', 'translateY(' + (($(window).scrollTop() / 8) - 270) + 'px)');
             $('.improve_bg').css('transform', 'translateY(' + ($(window).scrollTop() / 9.5) + 'px)');
             // $('.img_0').css('transform', 'translateY(' + ($(window).scrollTop() / 8) + 'px)');
         } else {
