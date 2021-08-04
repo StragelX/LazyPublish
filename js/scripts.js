f12505012b219168c4e3a08270fd39c3126a9fc0
UkiePro <ukiepro@UkiePros-iMac.local>
2021-08-04 17:12:53 +0300

    g

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ $( document ).ready(function() {
             // $('.img_1').css('transform', 'translateY(' + (($(window).scrollTop() / 5.2) - 420) +'px)');
             $('.img_2').css('transform', 'translateY(' + (($(window).scrollTop() / 4.3) - 500) +'px)');
             // $('.img_3').css('transform', 'translateY(' + (($(window).scrollTop() / 5) - 650) +'px)');
-            $('.img_4').css('transform', 'translateY(' + (($(window).scrollTop() / 4) - 500) +'px)');
+            $('.img_4').css('transform', 'translateY(' + (($(window).scrollTop() / 8) - 500) +'px)');
             // $('.img_5').css('transform', 'translateY(' + (($(window).scrollTop() / 6) - 400) +'px)');
         } else {
             $('#bg_text_1, #bg_text_2').removeAttr("style");
