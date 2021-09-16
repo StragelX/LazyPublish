65ea678c9ff7e54358c2dbb2a8da935cbd456322
StragelX <stragelx@gmail.com>
2021-09-16 10:44:42 +0300

    f

--- js/scripts.js
+++ js/scripts.js
@@ -9,23 +9,8 @@ $(document).ready(function () {
         "transform",
         "translateX(-" + (1000 - $(window).scrollTop() * 0.2) + "px)"
       );
-      // $('.img_4').css('transform', 'translateY(' + (($(window).scrollTop() / 8) - 270) + 'px)');
-      $(".improve_bg").css(
-        "transform",
-        "translateY(" + $(window).scrollTop() / 9.5 + "px)"
-      );
-
-      // if ( $(window).width() == 1024 ) {
-      //     $('.img_2').css('transform', 'translateY(' + (($(window).scrollTop() / 4.3) - 550) + 'px)');
-      // } else if ( $(window).width() == 768 ) {
-      //     $('.img_2').css('transform', 'translateY(' + (($(window).scrollTop() / 4.3) - 630) + 'px)');
-      // } else if ( $(window).width() <= 450 ) {
-      //     $('.img_2').css('transform', 'translateY(' + (($(window).scrollTop() / 4.3) - 770) + 'px)');
-      // } else {
-      //     $('.img_2').css('transform', 'translateY(' + (($(window).scrollTop() / 4.3) - 430) + 'px)');
-      // }
     } else {
-      $("#bg_text_1, .improve_bg, .img_0").removeAttr("style");
+      $("#bg_text_1").removeAttr("style");
     }
   });
 });
